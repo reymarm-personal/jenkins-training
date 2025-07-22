34b708e01c0d94c2a5146ab2363ba18aab1563a4
reymarm-personal <reymar.mercado.deltek@gmail.com>
2025-07-22 14:15:22 +0800

    Update fibonacci.ps1

--- jenkins-scripts/jenkins-github-fibonacci/scripts/fibonacci.ps1
+++ jenkins-scripts/jenkins-github-fibonacci/scripts/fibonacci.ps1
@@ -2,11 +2,13 @@ param(
     [int]$N = 10
 )
 
+# Initialize variables
 $a = 0
 $b = 1
 
 Write-Host "The Fibonacci series is :"
 
+# Generate Fibonacci sequence
 for ($i = 0; $i -lt $N; $i++) {
     Write-Host "$i`t$a"
     $fn = $a + $b
